a4a650257d9219d59f85dbc466cfa77bd5e44b9d
Kousik Satish <kousiksatish@gmail.com>
2019-11-05 00:29:54 +0530

    Add react store to the project

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.js';
+import configureStore from './redux/configureStore';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+
+ReactDOM.render(
+    <Provider store = {store}>
+        <App />  
+    </Provider>, 
+    document.getElementById('root')
+);
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,12 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 
 export default function configureStore(initiaState) {
-    return createStore(rootReducer, initiaState);
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    return createStore(
+        rootReducer, 
+        initiaState, 
+        composeEnhancers(applyMiddleware(reduxImmutableStateInvariant()))
+    );
 }
